aae531f1949d35817211c7d5a590f3014d559242
Elena Klepova <elena.klyopova@gmail.com>
2023-10-31 23:26:57 +0300

    Homework Lesson №21 Mobile Automation Browserstack

--- src/test/java/tests/IosTests.java
+++ src/test/java/tests/IosTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
 public class IosTests extends TestBase {
@@ -16,11 +17,11 @@ public class IosTests extends TestBase {
     @DisplayName("Проверка результата поиска Quality Assurance")
     void iosSearchTest() {
         step("Ввод в поле Quality assurance", () -> {
-            $(accessibilityId("Text Button")).click();
-            $(accessibilityId("Text Input")).sendKeys("Quality assurance");
+            $(id("Text Button")).click();
+            $(id("Text Input")).sendKeys("Quality assurance");
         });
         step("Проверка отображения результатов", () -> {
-            $(accessibilityId("Text Output")).shouldHave(Condition.text("Quality assurance"));
+            $(id("Text Output")).shouldHave(Condition.text("Quality assurance"));
         });
     }
 }
